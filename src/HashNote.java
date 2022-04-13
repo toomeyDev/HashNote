7a906404f5dba9ecd3211f4499cde4db4058aad1
Matthew Toomey <mtoomeydev@gmail.com>
2022-04-13 13:35:51 -0400

    Add methods for taking user-input to generate key/value entries as notes

--- src/HashNote.java
+++ src/HashNote.java
@@ -3,6 +3,7 @@
  * (listing out all notes stored in the noteFiles hash)
  */
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class HashNote {
 	// store notes in a local hashmap instance
@@ -10,8 +11,33 @@ public class HashNote {
 	
 	public HashNote() {
 		noteFiles = new HashMap<String, String>();
-		noteFiles.put("Sample Note", "This is a sample note, intended to test this feature.");
-		noteFiles.put("Test Note", "This test note is intended as dummy-data to test HashNote features.");
+		writeNote("Sample Note", "This is a sample note for testing HashNote functionality.");
+	}
+	
+	/* 
+	 * Write a note to the current noteFiles HashMap, prompting for name and 
+	 * content as user input taken from STDIN, give appropriate feedback to user
+	 */
+	public void writeNote() {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter note name: ");
+		// store note name to be used as hash key based on user input
+		String noteName = input.nextLine();
+		
+		System.out.print("Write note content: ");
+		String noteContent = input.nextLine();
+		
+		System.out.println("Your note:\n" + noteName + " : " + noteContent);
+		noteFiles.put(noteName, noteContent);
+		System.out.println("Note " + noteName + " saved successfully.");
+		
+		input.close();
+	}
+		
+	// Write a note to the current noteFiles HashMap, taking name and content as arguments
+	public void writeNote(String noteName, String noteContent) {
+		noteFiles.put(noteName, noteContent);
 	}
 	
 	@Override
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// create instance of HashNote type to test functionality
 		HashNote notes = new HashNote();
+		notes.writeNote();
 		System.out.println(notes);
 	}
 }
